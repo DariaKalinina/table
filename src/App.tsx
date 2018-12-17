64fb28c0d458c6ea6df49e768af251eaee7ee6e1
Daria Kalinina <lynxx91@mail.ru>
2018-12-17 15:07:18 +0300

    create template by create-react-app

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Table from './components/Table';
+
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="app">
+        <Header/>
+        <Table/>
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Table.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class Table extends React.Component {
+    public render() {
+        return (
+            <div className="table">
+                Table
+            </div>
+        );
+    }
+}
+
+export default Table;
