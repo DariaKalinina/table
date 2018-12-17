99791528456860cad000f20fa11c02737d0a9b16
Daria Kalinina <lynxx91@mail.ru>
2018-12-17 15:07:18 +0300

    create еуьзtemplate by create-react-app

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class Header extends React.Component {
+    public render() {
+        return (
+            <div className="header">
+                Header
+            </div>
+        );
+    }
+}
+
+export default Header;
---
+++ src/components/Table.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class Table extends React.Component {
+    public render() {
+        return (
+            <div className="header">
+                Table
+            </div>
+        );
+    }
+}
+
+export default Table;
